397e00073db03a991f15aac431ee394519bf1bb6
ChrisKnowsDev <chrisharley81@gmail.com>
2020-08-26 19:54:07 -0400

    added spinner and search functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState(' ');
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const res = await fetch(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
+      const data = await res.json();
+      setItems(data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, [query]);
+
   return (
     <div className='container'>
       <Header />
+      <Search getQuery={q => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
